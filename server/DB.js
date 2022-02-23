e344f3222a8a63a68844ab1614f25c277fd279d9
mo7medHasan <mo7med.hasan@gmail.com>
2022-02-23 18:38:26 +0200

    connect server & connect Databasen 23/2 6:38

--- server/DB.js
+++ server/DB.js
@@ -1,11 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL)
-.then(()=>{
-    console.log(`Success connect to Database`);
-}).catch((err)=>{
-    console.log(err);
-})
-exports.mongoose;
+mongoose.exports = async () => {
+  try {
+    const connectionParams = {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    };
+    await mongoose
+      .connect(process.env.MONGO_URL)
+      .then(() => {
+        console.log(`Success connect to Database 👏🤩😎`);
+      })
+      .catch((err) => {
+        console.log(`😥😡${err}`);
+      });
+  } catch (error) { console.log(`😥😣 ${error}`);}
+};
--- server/index.js
+++ server/index.js
@@ -1,2 +1,18 @@
 const express =require('express');
 const app =express();
+const cors =require('cors');
+
+
+app.use(express.json())
+app.use(cors())
+
+require("./DB");
+
+
+
+
+
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log(`server listen on ${process.env.PORT}  👌👏`)
+})
